bef1a4385152b79a33ee693dfbe8dc067f26d1c5
ratoncaton <ratoncaton12@gmail.com>
2023-05-11 20:17:48 +0200

    encriptacion y printeos

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import pickle
 from time import sleep
 import hashlib
+import os
 
 #Diccionari model d'usuari
 user = {
@@ -21,26 +22,43 @@ move_folders_destination = {
         "destination_folder": ""
 }
 
+######################################################################
+
+#funcio per netejar la terminal independentment del SO
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+#funcio per al titol
+def creation_title(new_user):
+    print("----- CONFIGURACIO DE USUARI -----")
+
+    print("Usuari: {}".format(new_user["username"]))
+
+    print("----------------------------------")
+    print()
+
 #Funcio per a crear usuaris 
 def create_user():
     
     #Es copia el diccionari model per a poder tenir mes de un usuari
     new_user = user.copy()
-    
-    print("----- CONFIGURACIO DE USUARI -----")
 
-    print("Usuari: {}".format(new_user["username"]))
+    creation_title(new_user)
 
-    
-
+    #os.system('cls' if os.name == 'nt' else 'clear')
     sleep(2)
     new_user["username"] = input("Nom de l'usuari: ")
+    
+
+    creation_title(new_user)
 
     sleep(2)
     new_user["passwd"] = encryption_hash()
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+####################################################################
     
-
+#Funcio de encriptacio hash per a guardar i verificar les contrasenyes de forma segura
 def encryption_hash():
 
     #es pregunta la contrasenya 
@@ -59,6 +77,8 @@ def encryption_hash():
     result = hash_code.hexdigest()
     return result
 
+def create_origin_folder(new_user):
+    new_user["move_folders"]
 
-
-
+if __name__ == "__main__":
+    create_user()
