9eac7215aff01f3556c1892248843904eb9e0def
ratoncaton <ratoncaton12@gmail.com>
2023-05-16 22:03:19 +0200

    functional login

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-import json
 import shutil
 import readchar
 import config
+from time import sleep
 
 actual_user = {}
 
 users = config.extract_users()
-print(users)
+#print(users)
 
 def choose_actual_user():
-    print("Quin usuari vols triar?")
-    print("")
+    
+    config.clear()
+
+    print(""" 
+---------- Inici Sessio ----------
+------------ usuaris -------------
+    """)
+
+
+    for user in range(len(users)):
+        print("{}.{} |".format(user, users[user]["username"]), end=" ")
+    
+    print("\n")
+    print("----------------------------------")
+    print("\nQui ets? (Prem el numero d'usuari)\n")
+    
+    user_choosen = readchar.readchar()
+
+    actual_user  = users[int(user_choosen)]
+    
+    password_actual_user = config.encryption_hash()
+
+    if password_actual_user == actual_user["passwd"]:
+        config.clear()
+        menu()
+    else:
+        config.clear()
+        print("Contrasenya incorrecta")
+        sleep(1)
+
+        choose_actual_user()
+
+
+choose_actual_user()
 
 def menu():
     print("""" 
