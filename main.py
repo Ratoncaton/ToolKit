bc0b6eb675357ff33cd7e6ec5e923cbd355cc499
ratoncaton <ratoncaton12@gmail.com>
2023-05-16 10:55:04 +0200

    added Main function choose user

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import shutil
 import readchar
 import config
 
+actual_user = {}
+
+def choose_actual_user():
+    pass
 
 def menu():
     print("""" 
@@ -15,9 +19,17 @@ def menu():
          $$ |\$$$$$$  |\$$$$$$  |$$ |$$ | \$$\ $$ |  \$$$$  |
          \__| \______/  \______/ \__|\__|  \__|\__|   \____/ 
 
+------------------------------------------------------------------
+
+Usuari: {} 
+
+------------------------------------------------------------------
+
 1. Ordenar Fitxers
 2. Acortador de links
 3. Netejar Cache
 4. Generador de contrasenyes
 5. Guardar contrasenyes
-    """)
+    """.format(actual_user["username"]))
+
+
